5832637a980dd55883424db159cc5e26cedb710a
zengei09 <federico.anon.zengei@gmail.com>
2019-03-16 05:42:35 +0800

    added calculator container

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!-- 
       created by:
-        Fedz 
+        Charissa Arellano 
 -->
 <!-- bootstrap connection and script using bootstrap 4.0-->
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
@@ -35,14 +35,14 @@
       function check(form) {
         form.userid.value == "q" && form.pswrd.value == "q"
         ? 
-        window.history.pushState('page2', 'Title', '/page2.php') 
+        window.history.pushState('calculator', 'Title', 'containers/calculator.php') 
         : 
         alert("Error Password or Username")
         ;
       }
 
     </script>
-    <script src="jquery/index.js"></script>
+    <script src="javascript/index.js"></script>
     <!-- Remind Passowrd -->
     <div id="formFooter">
       <a class="underlineHover" href="#">Forgot Password?</a>
@@ -50,43 +50,3 @@
 
   </div>
 </div>
-
-
-
-
-<!-- 
-    <html>
-    <head>
-    <title>
-    Login page
-    </title>
-    </head>
-    <body>
-    <h1 style="font-family:Comic Sans Ms;text-align="center";font-size:20pt;
-    color:#00FF00;>
-    Simple Login Page
-    </h1>
-    <form name="login">
-    Username<input type="text" name="userid"/>
-    Password<input type="password" name="pswrd"/>
-    <input type="button" onclick="check(this.form)" value="Login"/>
-    <input type="reset" value="Cancel"/>
-    </form>
-    <script language="javascript">
-    function check(form)/*function to check userid & password*/
-    {
-     /*the following code checkes whether the entered userid and password are matching*/
-     if(form.userid.value == "myuserid" && form.pswrd.value == "mypswrd")
-      {
-        window.open('target.html')/*opens the target page while Id & password matches*/
-      }
-     else
-     {
-       alert("Error Password or Username")/*displays error message*/
-      }
-    }
-    </script>
-    </body>
-    </html>
-
- -->
